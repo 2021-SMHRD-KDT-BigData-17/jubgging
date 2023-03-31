efdd7bcc724a50fe699b281d32f498fda6d59cd7
nano0 <nano0@172.30.1.65>
2023-03-31 09:41:09 +0900

    오전 1차

--- gittest/src/main/java/com/jubging/controller/ComUplodCon.java
+++ gittest/src/main/java/com/jubging/controller/ComUplodCon.java
@@ -16,23 +16,32 @@ public class ComUplodCon extends HttpServlet {
 	protected void service(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		
+		System.out.println("[ProfileCon]");
 		
+		// 0. post방식 인코딩
+		request.setCharacterEncoding("UTF-8");
+		// 1. 파라미터 수집
 		String user_id = request.getParameter("user_id");
 		String c_content = request.getParameter("c_content");
 		String c_hashtag = request.getParameter("c_hashtag");
 		String c_date = request.getParameter("c_date");
 		String c_img = request.getParameter("c_img");
 		
+		
+		// 2. 테이블 Community 객체에 담아주기
 		Community community = new Community(c_content,c_hashtag,user_id,c_img,c_date);
 		
+		
+		// 3-1. sql문 작성
+		// 3-2. DAO 메소드 생성
+		// 3-3. DAO 객체 생성
 		CommunityDAO dao = new CommunityDAO();
 		
-		Community com = dao.selectcommunity(community);
+		int com = dao.Insertuplod(community);
 		
-		
-		if(com != null) {
+		if(com > 0) {
 			System.out.println("타임라인 데이터 불러오기 성공!!");
-			System.out.println(com.toString());
+			/* System.out.println(com.toString()); */
 		}else {
 			System.out.println("타임라인 데이터 불러오기 실패!!");
 		}
--- gittest/src/main/java/com/jubging/domain/CommunityDAO.java
+++ gittest/src/main/java/com/jubging/domain/CommunityDAO.java
@@ -1,5 +1,7 @@
 package com.jubging.domain;
 
+import java.util.List;
+
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 
@@ -33,7 +35,7 @@ public class CommunityDAO {
 	} // insertMember 끝
 
 	
-	// 커뮤니티 타임라인 데이터 불러오기 
+	// 커뮤니티 타임라인 데이터 넣기
 	public Community selectcommunity(Community community) {
 
 		Community com = null;
@@ -47,4 +49,38 @@ public class CommunityDAO {
 		return com;
 	} // selectMember 끝
 
+	public int Insertuplod(Community newPost) {
+		int cnt = 0;
+		try {
+			cnt = sqlSession.insert("insertcommunity", newPost);
+			if (cnt > 0) {
+				System.out.println("DAO에서는 저장 성공");
+				sqlSession.commit();
+			} else {
+				sqlSession.rollback();
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			sqlSession.close();
+		} // finally 끝TODO Auto-generated method stub
+		return 0;
+	}
+	
+	
+	// 프로필 포스트창 타임라인 출력
+	public List<Community> SelectMember(String user_id) {
+		List<Community> cnt = null;
+
+		try {
+			cnt = sqlSession.selectList("com.jubging.darabase.JubgingMapper1.SelectMember", user_id);
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			sqlSession.close();
+		}
+		return cnt;
+
+	}
+	
 }
